58eda929599c2e28e29456a0bf7cd2de2b71469a
서순호 <dnsdhrj123@naver.com>
2016-01-11 21:08:14 +0900

    Add movement in Daram

--- Assets/Scripts/Daram.cs
+++ Assets/Scripts/Daram.cs
@@ -11,6 +11,7 @@ public class Daram : MonoBehaviour {
     
     public int Level;
     public int InitialHP;
+    public int Speed;
 
     public int HP {
         get { return _HP; }
@@ -18,6 +19,7 @@ public class Daram : MonoBehaviour {
     }
     
     private int _HP = 0;
+    private Vector2 dir;
 
     // *중요*
     // Daram 하위 클래스의 Start()에 "base.Start();" 를 넣어주세요.
@@ -29,10 +31,44 @@ public class Daram : MonoBehaviour {
         All.Add(this);
     }
 
+    // *중요*
+    // Update()도 위와 동일
+    protected void Update()
+    {
+        Move();
+    }
+
     public void Die()
     {
         All.Remove(this);
         Destroy(gameObject);
+    }
+
+    private float NextMove = 0;
+    void Move()
+    {
+        if (NextMove <= Time.time)
+        {
+            switch (Random.Range(0, 4))
+            {
+                case 0:
+                    dir = Vector2.up;
+                    break;
+                case 1:
+                    dir = Vector2.down;
+                    break;
+                case 2:
+                    dir = Vector2.left;
+                    break;
+                case 3:
+                    dir = Vector2.right;
+                    break;
+            }
+
+            NextMove = Time.time + Random.Range(1.0f, 3.0f);    //방향전환 시간
+        }
+
+        transform.position += (Vector3) dir * (Speed * 0.001f);
     }
 
     //All.Remove()를 쓰기 위해 비교연산자가 필요함.
