72780501b10f468a36509c71a1ebf6ff080c6128
Jhuni <whdgnsdl887@naver.com>
2016-01-12 17:27:35 +0900

    Edit logscrollbar

--- Assets/Scripts/UI/LogText.cs
+++ Assets/Scripts/UI/LogText.cs
@@ -5,17 +5,24 @@ using System.Collections;
 public class LogText : MonoBehaviour {
     
     private Text logText;
+
+    public GameObject scrollbar;
+    private Scrollbar logScrollbar;
     public string initialText = "The Game Start";
 	
     // Use this for initialization
-	void Start () 
+	void Awake () 
     {
         logText = GetComponent<Text>();
         logText.text = initialText;
+
+        logScrollbar = scrollbar.GetComponent<Scrollbar>();
+        logScrollbar.size = 0;
     }
 
     public void WriteLog(string log)
     {
         logText.text += "\n" + log;
+        logScrollbar.value = 0;
     }
 }
