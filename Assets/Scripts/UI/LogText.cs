3594b87ebbb3381b213dac46b5470ffe152b063c
Jhuni <whdgnsdl887@naver.com>
2016-01-12 16:39:45 +0900

    make Log Text

---
+++ Assets/Scripts/UI/LogText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LogText : MonoBehaviour {
+    
+    private Text logText;
+    public string initialText = "The Game Start";
+	
+    // Use this for initialization
+	void Start () 
+    {
+        logText = GetComponent<Text>();
+        logText.text = initialText;
+    }
+
+    public void WriteLog(string log)
+    {
+        logText.text += "\n" + log;
+    }
+}
