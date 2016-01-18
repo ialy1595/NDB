70920a702402180dcdc1ff822878528e0e9271f4
서순호 <dnsdhrj123@naver.com>
2016-01-19 00:10:35 +0900

    EventBoxTemplate 추가

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -7,6 +7,7 @@ public class Events : MonoBehaviour {
     private GameManager gm; //많이 쓸것같아서 만들어둠
 
     public GameObject UnlockUpBasic_Button;
+    public GameObject UnlockUpBasic_Box;
 
     void Start ()
     {
@@ -20,9 +21,12 @@ public class Events : MonoBehaviour {
         if (GameManager.gm.Fame >= 5000)
         {
             GameManager.gm.EventCheck -= UnlockUpBasic;
-            LogText.WriteLog("인기에 힘입어 LV.2 다람쥐를 개발했다!");
+ 
             UnlockUpBasic_Button.GetComponent<Button>().interactable = true;
+            Instantiate(UnlockUpBasic_Box);
+            LogText.WriteLog("인기에 힘입어 LV.2 다람쥐를 개발했다!");
             UserChat.CreateChat("GM: 고레벨 다람쥐가 새롭게 등장합니다!!", 5);
+            
             gm.FameChange += gm.FameDaram2;
             gm.UserChange += gm.UserLevel2;
             gm.EventCheck += UserChat.uc.Daram2Number;
---
+++ Assets/Scripts/UI/EventBox.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EventBox : MonoBehaviour {
+
+    void Start()
+    {
+        transform.SetParent(GameObject.Find("Canvas").transform);
+        GetComponent<RectTransform>().localPosition = Vector3.zero;
+        GameManager.gm.Pause();
+    }
+
+    public void OnClick()
+    {
+        GameManager.gm.Pause();
+        Destroy(gameObject);
+    }
+}
