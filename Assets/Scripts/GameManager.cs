d6223e025b82b8a5093f23a3c8c0a69ddc5acc30
서순호 <dnsdhrj123@naver.com>
2016-01-23 00:06:49 +0900

    프레임 문제 해결

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -41,6 +41,8 @@ public class GameManager : MonoBehaviour {
 
     void Awake()
     {
+        Application.targetFrameRate = 60;
+
         gm = this;
         resultScene = GameObject.Find("ResultScene");
         resultScene.SetActive(false);
