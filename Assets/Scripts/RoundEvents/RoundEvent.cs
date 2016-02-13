5e93ba155bf66e28876a61228e87bfeec064f1fc
서순호 <dnsdhrj123@naver.com>
2016-02-14 02:46:59 +0900

    이벤트 코드 구현

--- Assets/Scripts/RoundEvents/RoundEvent.cs
+++ Assets/Scripts/RoundEvents/RoundEvent.cs
@@ -11,9 +11,11 @@ public class RoundEvent{
     public int eventRequiredDev;
     public string eventDescription;
     public Texture2D eventImage;
+    public delegate void VoidFunction();
+    public VoidFunction eventStart;
 
     //이미지는 이벤트이름(eventName)과 똑같은 파일명을 가진 걸 자동으로 사용하도록 했음
-    public RoundEvent(string name, string funcname, int ID, int price, int dev, string desc)
+    public RoundEvent(string name, string funcname, int ID, int price, int dev, string desc, VoidFunction onStart)
     {
         eventName = name;
         eventFuncName = funcname;
@@ -22,6 +24,7 @@ public class RoundEvent{
         eventRequiredDev = dev;
         eventDescription = desc;
         eventImage = Resources.Load<Texture2D>("Event Icons/" + name);
+        eventStart = onStart;
     }
 
     public RoundEvent()
--- Assets/Scripts/RoundEvents/RoundEventDatabase.cs
+++ Assets/Scripts/RoundEvents/RoundEventDatabase.cs
@@ -6,11 +6,17 @@ using System.Linq;
 public class RoundEventDatabase : MonoBehaviour {
     public List<RoundEvent> roundEventDatabase = new List<RoundEvent>();
 
+    private GameManager gm;
+
     //라운드이벤트 데이터베이스는 여기에 추가
     void Start()
     {
-        roundEventDatabase.Add(new RoundEvent("경험치 두배 이벤트", "ExpEvent", 0, 1000, 3, "인기도 상승율이 증가합니다.\n 유저 수준이 올라갑니다."));
-        roundEventDatabase.Add(new RoundEvent("약속과 믿음", "Kiri", 1, 777, 5, "유저 수 증가율이 감소합니다."));
+        gm = GameManager.gm;
+
+        roundEventDatabase.Add(new RoundEvent("경험치 두배 이벤트", "ExpEvent", 0, 1000, 3, "인기도 상승율이 증가합니다.\n 유저 수준이 올라갑니다.",
+            delegate() { gm.FameChange += ExpEvent; gm.UserChange += ExpEvent2; } ));
+        roundEventDatabase.Add(new RoundEvent("약속과 믿음", "Kiri", 1, 777, 5, "유저 수 증가율이 감소합니다.",
+            delegate() { gm.UserChange += Kiri; } ));
         //roundEventDatabase.Add(new RoundEvent("헤파이스토스", 1, 888, "유저가 감소합니다."));
     }
 
@@ -43,35 +49,64 @@ public class RoundEventDatabase : MonoBehaviour {
         }
     }
 
-    //이렇게 안해도 되는 좋은 방법을 찾습니다
+    //이렇게 안해도 되는 좋은 방법을 찾습니다 -> 수정은 했는데 좋은 방법인지는 잘 모르겠음
     void FindRoundEvent(int id)
     {
-        switch (id)
+        /*switch (id)
         {
             case 0:
-                GameManager.gm.EventCheck += ExpEvent;
+                GameManager.gm.FameChange += ExpEvent;
+                GameManager.gm.UserChange += ExpEvent2;
                 break;
 
             case 1:
-                GameManager.gm.EventCheck += Kiri;
+                GameManager.gm.UserChange += Kiri;
                 break;
 
             default:
                 break;
-        }
+        }*/
+        roundEventDatabase[id].eventStart();
     }
 
     void ExpEvent()
     {
         Debug.Log("ExpEvent");
         if (GameManager.gm.isRoundEventOn == false)
-            GameManager.gm.EventCheck -= ExpEvent;
+            GameManager.gm.FameChange -= ExpEvent;
+
+        //인기도 30% 증가
+        gm.Fame += (int) (gm.DaramFunction[User.level1].value / 3.0f);
+        if(Unlockables.GetBool("UnlockDaram1") == true)
+            gm.Fame += (int)(gm.DaramFunction[User.level2].value / 3.0f);
     }
 
+    void ExpEvent2()
+    {
+        if (GameManager.gm.isRoundEventOn == false)
+            GameManager.gm.UserChange -= ExpEvent2;
+
+        //경험치가 두배니까 유저도 두배
+        gm.UserLevel2();
+    }
+
+    private int PrevUser = 0;
     void Kiri()
     {
         Debug.Log("Kiri");
         if (GameManager.gm.isRoundEventOn == false)
-            GameManager.gm.EventCheck -= Kiri;
+            GameManager.gm.UserChange -= Kiri;
+
+        //일단 중급 유저 증가량만 감소
+        if (PrevUser == 0)
+            PrevUser = gm.UserCount[User.level2];
+        else
+        {
+            int DeltaUser = gm.UserCount[User.level2] - PrevUser;
+            if(DeltaUser > 0)
+                gm.UserCount[User.level2] -= DeltaUser / 2;
+
+            PrevUser = gm.UserCount[User.level2];
+        }
     }
 }
