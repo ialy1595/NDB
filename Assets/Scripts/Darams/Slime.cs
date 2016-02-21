88ac5acc1e9c103c4ef12b3fb0564484f93e5c37
ialy <ialy12006@gmail.com>
2016-02-21 21:07:28 +0900

    fix anim error

--- Assets/Scripts/Darams/Slime.cs
+++ Assets/Scripts/Darams/Slime.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Basic : Daram {
+public class Slime : Daram
+{
 
     private int past_move_stat = 0; // 현재 움직임 상태
     private int now_move_stat; //이전 움직임 상태
 
-    
+
 
     void Start()
     {
@@ -18,21 +19,15 @@ public class Basic : Daram {
     void FixedUpdate()
     {
         now_move_stat = base.move_stat;
-        if (now_move_stat != past_move_stat)
+        if ((now_move_stat%2) != (past_move_stat%2))
         {
-            switch (now_move_stat)
+            switch (now_move_stat%2)
             {
                 case 0:
-                    Anim.SetTrigger("Up");
+                    Anim.SetTrigger("UL");
                     break;
                 case 1:
-                    Anim.SetTrigger("Down");
-                    break;
-                case 2:
-                    Anim.SetTrigger("Left");
-                    break;
-                case 3:
-                    Anim.SetTrigger("Right");
+                    Anim.SetTrigger("DR");
                     break;
             }
             past_move_stat = now_move_stat;
