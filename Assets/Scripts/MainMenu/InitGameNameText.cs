806b0b5c49bd8151ed19854b4b951686e1ca0dc8
Jhuni <whdgnsdl887@naver.com>
2016-02-25 06:37:38 +0900

    메인메뉴 UI 수정

---
+++ Assets/Scripts/MainMenu/InitGameNameText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InitGameNameText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Text myGameName = GetComponent<Text>();
+        myGameName.text = "내 게임 이름 : " + GameManager.gm.GameName;
+	}
+	
+	
+}
